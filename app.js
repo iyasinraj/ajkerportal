0e48ea1f12fa2eef58b8925ec553916b64226971
Iyasin Raj <78220038+iyasinraj@users.noreply.github.com>
2023-04-04 21:04:22 +0600

    add nav and categories

---
+++ app.js
@@ -0,0 +1,19 @@
+const catagory = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCatagory(data.data.news_category);
+}
+
+const displayCatagory = (categoris) =>{
+    const categorisDiv = document.getElementById('categoris-div');
+    for(const category of categoris){
+        const categoryItem = document.createElement('p')
+        categoryItem.innerHTML = `<a class = "text-decoration-none fw-bold" href="http://">${category.category_name}</a>`;
+        categorisDiv.appendChild(categoryItem);
+    }
+}
+
+catagory();
+
+
